7344eaa612fa831f9900932eba2bd00106f94443
Wagner Carmo da Silva <wcsilva@viajanetsp.local>
2020-06-27 18:44:09 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Transactions;
+using System.Xml.Schema;
+
+namespace ConsoleApp12
+{
+    class Item
+    {
+        public string Descricao { get; private set; }
+        public double Valor { get; private set; }
+
+        public Item(string descricao, double valor)
+        {
+            this.Descricao = descricao;
+            this.Valor = valor;
+        }
+    }
+    class Orcamento
+    {
+        public string Descricao { get; private set; }
+        public List<Item> Items { get; private set; }
+        public double Total => this.Items.Sum(item => item.Valor);
+
+        public Orcamento(string descricao)
+        {
+            this.Descricao = descricao;
+            this.Items = new List<Item>();
+        }
+
+        public void AdicionaItem(Item item)
+        {
+            this.Items.Add(item);
+        }
+    }
+    interface IImposto
+    {
+        double CalculaImposto(Orcamento orcamento);
+    }
+
+    abstract class TemplateImposto : IImposto
+    {
+        public double CalculaImposto(Orcamento orcamento)
+        {
+            if (UsarValorMaximo(orcamento))
+                return CalcularMaximoImposto(orcamento);
+            else
+                return CalcularMenorImposto(orcamento);
+        }
+
+        protected abstract bool UsarValorMaximo(Orcamento orcamento);
+
+        protected abstract double CalcularMaximoImposto(Orcamento orcamento);
+
+        protected abstract double CalcularMenorImposto(Orcamento orcamento);
+    }
+
+    class ICPP : TemplateImposto
+    {
+        protected override double CalcularMaximoImposto(Orcamento orcamento)
+        {
+            return orcamento.Total * 0.07;
+        }
+
+        protected override double CalcularMenorImposto(Orcamento orcamento)
+        {
+            return orcamento.Total * 0.05;
+        }
+
+        protected override bool UsarValorMaximo(Orcamento orcamento)
+        {
+            return orcamento.Total > 500;
+        }
+    }
+
+    class IKVC : TemplateImposto
+    {
+        protected override double CalcularMaximoImposto(Orcamento orcamento)
+        {
+            return orcamento.Total * 0.1;
+        }
+
+        protected override double CalcularMenorImposto(Orcamento orcamento)
+        {
+            return orcamento.Total * 0.06;
+        }
+
+        protected override bool UsarValorMaximo(Orcamento orcamento)
+        {
+            return orcamento.Total > 500 && orcamento.Items.Any(item => item.Valor > 100);
+        }
+    }
+
+    class IHIT : TemplateImposto
+    {
+        protected override double CalcularMaximoImposto(Orcamento orcamento)
+        {
+            return (orcamento.Total * 0.13) + 100;
+        }
+
+        protected override double CalcularMenorImposto(Orcamento orcamento)
+        {
+            return orcamento.Total * 0.01;
+        }
+
+        protected override bool UsarValorMaximo(Orcamento orcamento)
+        {
+            return orcamento.Items.GroupBy(item => item.Descricao).Any(item => item.Count() > 1);
+        }
+    }
+
+    class Program
+    {
+        static void Mainxx(string[] args)
+        {
+            var orcamento = new Orcamento("Orcamento teste");
+            orcamento.AdicionaItem(new Item("item1", 100));
+
+            ExibiMensagens("ICPP, IKVC e IHIT no minimo", orcamento);
+
+            orcamento.AdicionaItem(new Item("item1", 600));
+
+            ExibiMensagens("ICPP, IKVC e IHIT no máximo", orcamento);
+        }
+
+        static void ExibiMensagens(string titulo, Orcamento orcamento)
+        {
+            var icpp = new ICPP();
+            var ikvc = new IKVC();
+            var ihit = new IHIT();
+
+            Console.WriteLine(titulo);
+            Console.WriteLine("Total Orcamento: {0}", orcamento.Total);
+            Console.WriteLine("ICPP: {0}", icpp.CalculaImposto(orcamento));
+            Console.WriteLine("IKVC: {0}", ikvc.CalculaImposto(orcamento));
+            Console.WriteLine("IHIT: {0}", ihit.CalculaImposto(orcamento));
+
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Relatorio.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace TempleteMethod
+{
+    class Conta
+    {
+        public string Nome { get; private set; }
+        public double Saldo { get; private set; }
+
+        public string Agencia { get; private set; }
+        public string Numero { get; private set; }
+
+        public Conta(string nome, double saldo, string agencia, string numero)
+        {
+            this.Nome = nome;
+            this.Saldo = saldo;
+            this.Agencia = agencia;
+            this.Numero = numero;
+        }
+    }
+
+    class Banco
+    {
+        public string Nome { get; private set; }
+
+        public string Telefone { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Endereco { get; private set; }
+
+        public Banco(string nome, string telefone, string email, string endereco)
+        {
+            this.Nome = nome;
+            this.Telefone = telefone;
+            this.Email = email;
+            this.Endereco = endereco;
+        }
+
+    }
+
+    abstract class RelatorioTemplate
+    {
+        protected abstract void Cabecalho(Banco banco);
+
+        protected abstract void Detalhe(List<Conta> contas);
+
+        protected abstract void Rodape(Banco banco);
+
+        public void ImprimeRelatorio(Banco banco, List<Conta> contas)
+        {
+            Cabecalho(banco);
+            Detalhe(contas);
+            Rodape(banco);
+        }
+    }
+
+    class RelatorioSimples : RelatorioTemplate
+    {
+        protected override void Cabecalho(Banco banco)
+        {
+            Console.WriteLine("Banco {0} Telefone: {1}", banco.Nome, banco.Telefone);
+            Console.WriteLine();
+        }
+
+        protected override void Detalhe(List<Conta> contas)
+        {
+            contas.ForEach(conta =>
+            {
+                Console.WriteLine("Nome: {0} Saldo: {1}", conta.Nome, conta.Saldo);
+            });
+        }
+
+        protected override void Rodape(Banco banco)
+        {
+            Console.WriteLine();
+            Cabecalho(banco);
+        }
+    }
+
+    class RelatorioComplexo : RelatorioTemplate
+    {
+        protected override void Cabecalho(Banco banco)
+        {
+            Console.WriteLine("Banco: {0} ", banco.Nome);
+            Console.WriteLine("Endereço: {0} Telefone: {1}", banco.Endereco, banco.Telefone);
+            Console.WriteLine();
+        }
+
+        protected override void Detalhe(List<Conta> contas)
+        {
+            contas.ForEach(conta =>
+            {
+                Console.WriteLine("Agêencia: {2} Conta: {3} Nome: {0} Saldo: {1}", conta.Nome, conta.Saldo, conta.Agencia, conta.Numero);
+            });
+        }
+
+        protected override void Rodape(Banco banco)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Email: {0} Data e Hora: {1}", banco.Email, DateTime.Now);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var contas = new List<Conta>()
+            {
+                new Conta("Cliente 1", 100, "1", "1-1"),
+                new Conta("Cliente 2", 200, "2", "2-2"),
+                new Conta("Cliente 3", 300, "3", "3-3")
+            };
+
+            var banco = new Banco("Teste1", "0030-04390", "cc@teste1.com", "R.xxxx, 292");
+
+            var relatorioSimples = new RelatorioSimples();
+            relatorioSimples.ImprimeRelatorio(banco, contas);
+
+            var relatorioComplexo = new RelatorioComplexo();
+            relatorioComplexo.ImprimeRelatorio(banco, contas);
+        }
+    }
+}

fe9b7f67236ae092bcf91ac6f591f57406e203b8
wagnercds <wagnercds1978@gmail.com>
2020-06-28 19:05:09 -0300

    Resposta dos exercicios sobre o padrão state

---
+++ State/Conta.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace State
+{
+    class ContaConsole
+    {
+
+        interface IStatusConta
+        {
+            (IStatusConta statuAtual, double saldo) Deposita(Conta conta, double valor);
+            (IStatusConta statuAtual, double saldo) Saque(Conta conta, double valor);
+        }
+
+        class StatusNegativo : IStatusConta
+        {
+            public (IStatusConta statuAtual, double saldo) Deposita(Conta conta, double valor)
+            {
+                var saldo = conta.Saldo + (valor * 0.95);
+                var statusAtual = saldo < 0 ? (IStatusConta) this : (IStatusConta) new StatusPositivo();
+
+                return (statusAtual, saldo);
+            }
+
+            public (IStatusConta statuAtual, double saldo) Saque(Conta conta, double valor)
+            {
+                throw new Exception("Saldo da conta negativo");
+            }
+
+            public override string ToString()
+            {
+                return "Negativo";
+            }
+        }
+
+        class StatusPositivo : IStatusConta
+        {
+            public (IStatusConta statuAtual, double saldo) Deposita(Conta conta, double valor)
+            {
+                var saldo = conta.Saldo + (valor * 0.98);
+
+                return (StatusAtual(saldo), saldo);
+            }
+
+            public (IStatusConta statuAtual, double saldo) Saque(Conta conta, double valor)
+            {
+                var saldo = conta.Saldo - valor;
+
+                return (StatusAtual(saldo), saldo);
+            }
+
+            private IStatusConta StatusAtual(double saldo)
+            {
+                return saldo < 0 ? (IStatusConta)new StatusNegativo() : (IStatusConta)this;
+            }
+
+            public override string ToString()
+            {
+                return "Positivo";
+            }
+        }
+
+        class Conta
+        {
+            public string Nome { get; private set; }
+            public double Saldo { get; private set; }
+
+            public string Agencia { get; private set; }
+            public string Numero { get; private set; }
+
+            public DateTime Abertura { get; private set; }
+
+            public IStatusConta Status { get; private set; }
+
+            public Conta(string nome, double saldo, string agencia, string numero, DateTime abertura)
+            {
+                this.Nome = nome;
+                this.Saldo = saldo;
+                this.Agencia = agencia;
+                this.Numero = numero;
+                this.Abertura = abertura;
+
+                if (this.Saldo < 0)
+                    this.Status = new StatusNegativo();
+                else
+                    this.Status = new StatusPositivo();
+            }
+
+            public void Deposita(double valor)
+            {
+                (this.Status, this.Saldo) = this.Status.Deposita(this, valor);
+            }
+
+            public void Saque(double valor)
+            {
+                (this.Status, this.Saldo) = this.Status.Saque(this, valor);
+            }
+        }
+        static void Main(string[] args)
+        {
+            var conta = new Conta("conta", 100, "1", "2002", DateTime.Now);
+            Console.WriteLine("Saldo: {0} - Status: {1}", conta.Saldo, conta.Status);
+
+            conta.Saque(200);
+            Console.WriteLine("Saque 200");
+            Console.WriteLine("Saldo: {0} - Status: {1}", conta.Saldo, conta.Status);
+
+            conta.Deposita(100);
+            Console.WriteLine("Deposita 100");
+            Console.WriteLine("Saldo: {0} - Status: {1}", conta.Saldo, conta.Status);
+
+            conta.Deposita(100);
+            Console.WriteLine("Deposita 100");
+            Console.WriteLine("Saldo: {0} - Status: {1}", conta.Saldo, conta.Status);
+
+            conta.Saque(50);
+            Console.WriteLine("Saque 50");
+            Console.WriteLine("Saldo: {0} - Status: {1}", conta.Saldo, conta.Status);
+        }
+    }
+}
---
+++ State/Orcamento.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace State
+{
+    interface IStatusOrcamento
+    {
+        double CalculaDesconto(Orcamento orcamento);
+        IStatusOrcamento Aprova();
+        IStatusOrcamento Reprova();
+        IStatusOrcamento Finaliza();
+    }
+
+    class StatusAprovado : IStatusOrcamento
+    {
+        public IStatusOrcamento Aprova()
+        {
+            throw new Exception("Orçamento já está aprovado !");
+        }
+
+        public double CalculaDesconto(Orcamento orcamento)
+        {
+            return orcamento.Total * 0.02;
+        }
+
+        public IStatusOrcamento Finaliza()
+        {
+            return new StatusFinalizado();
+        }
+
+        public IStatusOrcamento Reprova()
+        {
+            throw new Exception("Orçamento não pode ser mais reprovado !");
+        }
+
+        public override string ToString()
+        {
+            return "Aprovado";
+        }
+    }
+
+    class StatusEmAprovacao : IStatusOrcamento
+    {
+        public IStatusOrcamento Aprova()
+        {
+            return new StatusAprovado();
+        }
+
+        public double CalculaDesconto(Orcamento orcamento)
+        {
+            return orcamento.Total * 0.05;
+        }
+
+        public IStatusOrcamento Finaliza()
+        {
+            throw new Exception("Orçamento não pode ser finalizado !");
+        }
+
+        public IStatusOrcamento Reprova()
+        {
+            return new StatusReprovado();
+        }
+        public override string ToString()
+        {
+            return "Em Aprovação";
+        }
+    }
+
+    class StatusReprovado : IStatusOrcamento
+    {
+        public IStatusOrcamento Aprova()
+        {
+            throw new Exception("Orçamento não pode ser mais aprovado !");
+        }
+
+        public double CalculaDesconto(Orcamento orcamento)
+        {
+            return 0;
+        }
+
+        public IStatusOrcamento Finaliza()
+        {
+            return new StatusFinalizado();
+        }
+
+        public IStatusOrcamento Reprova()
+        {
+            throw new Exception("Orçamento já foi reprovado !");
+        }
+
+        public override string ToString()
+        {
+            return "Reprovado";
+        }
+    }
+
+    class StatusFinalizado : IStatusOrcamento
+    {
+        public IStatusOrcamento Aprova()
+        {
+            throw new Exception("Orçamento já está finalizado !");
+        }
+
+        public double CalculaDesconto(Orcamento orcamento)
+        {
+            return 0;
+        }
+
+        public IStatusOrcamento Finaliza()
+        {
+            throw new Exception("Orçamento já está finalizado !");
+        }
+
+        public IStatusOrcamento Reprova()
+        {
+            throw new Exception("Orçamento já está finalizado !");
+        }
+
+        public override string ToString()
+        {
+            return "Finalizado";
+        }
+    }
+
+    class Item
+    {
+        public string Descricao { get; private set; }
+        public double Valor { get; private set; }
+
+        public Item(string descricao, double valor)
+        {
+            this.Descricao = descricao;
+            this.Valor = valor;
+        }
+    }
+    class Orcamento
+    {
+        public string Descricao { get; private set; }
+        public List<Item> Items { get; private set; }
+        public double Total => this.Items.Sum(item => item.Valor);
+        public double Desconto => StatusOrcamento.CalculaDesconto(this);
+        public double TotalComDesconto => this.Total - this.Desconto;
+
+        public IStatusOrcamento StatusOrcamento { get; private set; }
+
+        public Orcamento(string descricao)
+        {
+            this.Descricao = descricao;
+            this.Items = new List<Item>();
+            this.StatusOrcamento = new StatusEmAprovacao();
+        }
+
+        public void AdicionaItem(Item item)
+        {
+            this.Items.Add(item);
+        }
+
+        public void Aprova()
+        {
+            this.StatusOrcamento = this.StatusOrcamento.Aprova();
+        }
+
+        public void Reprova()
+        {
+            this.StatusOrcamento = this.StatusOrcamento.Reprova();
+        }
+
+        public void Finaliza()
+        {
+            this.StatusOrcamento = this.StatusOrcamento.Finaliza();
+        }
+
+        
+    }
+
+    class OrcamentoConsole
+    {
+        static void Mainx(string[] args)
+        {
+            var orcamento = new Orcamento("Orcamento teste");
+            orcamento.AdicionaItem(new Item("item1", 100));
+
+            Console.WriteLine("Status Orcamento: {0} - Total: {2} - Desconto: {1} - Total com Desconto {3}", orcamento.StatusOrcamento, orcamento.Desconto, orcamento.Total, orcamento.TotalComDesconto);
+
+            orcamento.Aprova();
+
+            Console.WriteLine("Status Orcamento: {0} - Total: {2} - Desconto: {1} - Total com Desconto {3}", orcamento.StatusOrcamento, orcamento.Desconto, orcamento.Total, orcamento.TotalComDesconto);
+
+            orcamento.Finaliza();
+
+            Console.WriteLine("Status Orcamento: {0} - Total: {2} - Desconto: {1} - Total com Desconto {3}", orcamento.StatusOrcamento, orcamento.Desconto, orcamento.Total, orcamento.TotalComDesconto);
+
+
+        }
+    }
+}
